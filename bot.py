739c4081443e3c502af054020d0749489629129c
Danny Dyla <dyladan@gmail.com>
2014-03-10 02:07:30 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import bot.irc.connection as connection
+import bot.util.parse as parse
+import re
+import requests
+
+chan = "#bookie"
+nick = "bookiebot"
+server = "irc.freenode.net"
+
+def github_issue( issue, user="bookieio", repo="bookie" ):
+  r = requests.get("https://api.github.com/repos/%s/%s/issues/%s" % (user,repo,issue))
+  if r.status_code == 200:
+    json = r.json()
+    url = json['html_url']
+    title = json['title']
+    state = json['state']
+    return state + " - " + url + " - " + title
+  else:
+    return None
+
+def github(s):
+  match = re.search(r"(?:^|\s)#?(\d+)\b",s)
+  if match:
+      number = match.group(1)
+      return github_issue(number)
+  else:
+      return None
+
+irc = connection.Connection(server=server,nick=nick)
+irc.join("#test")
+while True:
+  out = parse.parsemsg(irc.listen())
+  prefix  = out[0]
+  command = out[1]
+  args    = out[2]
+  if command == 'PRIVMSG':
+    if args[0] == irc.nick:
+      channel = prefix.split('!')[0]
+    else:
+      channel = args[0]
+    message = args[1].rstrip()
+    ##irc.privmsg(channel,message)
+    issue = github(message)
+    if issue:
+      irc.privmsg(channel,issue)
+
---
+++ bot/irc/connection.py
@@ -0,0 +1,69 @@
+"""Manage connections with irc server
+
+Simple usage:
+>> import bot.irc.connection as connection
+>> irc = connection.Connection(server="irc.freenode.net", nick="bookiebot")
+>> irc.join(#channel)
+>> irc.listen()
+
+functions:
+__init__
+"""
+import socket
+import sys
+
+class Connection:
+  "Create a connection to an irc channel"
+  _p = sys.stdout.write
+  def __init__(self, server="irc.freenode.net", nick="bookiebot", name="Alan Turing", port=6667):
+    self.nick = nick
+    self.name = name
+    self.s = socket.socket()
+    self.connect(server, port, nick, name)
+
+  def connect(self, server, port=6667, nick="bookiebot", name="Alan Turing"):
+    "Connect and return a socket"
+    self.s.connect((server,port))
+    self.s.recv(4096)
+    self.setnick(nick)
+    self.setuser(name)
+
+  def quit(self, reason="GOOD BYE"):
+    data = "QUIT :Bye\r\n"
+    print ">", data.rstrip()
+    self.s.send(bytearray(data, "utf-8"))
+    self.s.close()
+    sys.exit(0)
+
+  def privmsg(self, chan,message):
+    data = 'PRIVMSG %s :%s\r\n' % (chan, message)
+    self.s.send(data)
+    self._p("> %s" % data)
+
+  def setnick(self, nick):
+    data = 'NICK %s\r\n' % nick
+    self.s.send(data)
+    self._p("> %s" % data)
+    self.nick = nick
+
+  def setuser(self, name):
+    data = 'USER %s 0 * :%s\r\n' % (self.nick, name)
+    self.s.send(data)
+    self._p("> %s" % data)
+    self.name = name
+
+  def join(self, chan):
+    data = 'JOIN %s\r\n' % chan
+    self.s.send(data)
+    self._p("> %s" % data)
+    self.privmsg(chan,"hello")
+
+  def listen(self):
+    while True: #While Connection is Active
+      data = self.s.recv (4096) #Make Data the Receive Buffer
+      if data[:4] == "PING":
+        pong = 'PONG %s' % data[6:]
+        self.s.send(pong) #Send back a PONG
+        self._p("> %s" % pong)
+      else:
+        return data
