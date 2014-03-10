01b4fb75c13c84ca1e7cbdd644c94d62ea6c34bc
Danny Dyla <dyladan@gmail.com>
2014-03-10 12:56:22 -0400

    added searching for functions in bookie

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
+from bot.util.find import find_func
 import bot.irc.connection as connection
 import bot.util.parse as parse
 import re
+import os
 import requests
 
-chan = ""
-nick = ""
-server = ""
+chan = "#bookietest"
+nick = "bookiebot"
+server = "irc.freenode.net"
+
+#os.chdir("/home/user/bookie/bookie")
+
+
+os.system("git clone http://github.com/bookieio/bookie.git")
+os.chdir("bookie/bookie")
 
 def github_issue( issue, user="bookieio", repo="bookie" ):
   r = requests.get("https://api.github.com/repos/%s/%s/issues/%s" % (user,repo,issue))
@@ -33,13 +41,24 @@ while True:
   prefix  = out[0]
   command = out[1]
   args    = out[2]
+  print prefix, command
   if command == 'PRIVMSG':
     if args[0] == irc.nick:
       channel = prefix.split('!')[0]
     else:
       channel = args[0]
     message = args[1].rstrip()
-    ##irc.privmsg(channel,message)
+    if re.match(".func \w+", message):
+      os.system("git pull")
+      l = find_func(message[6:].rstrip())
+      out = [item for sublist in l for item in sublist]
+      if len(out) > 5:
+        irc.privmsg(channel,"More than 5 results. Please refine your search.")
+      else:
+        for s in out[:5]:
+          irc.privmsg(channel,s)
+      continue
+
     issue = github(message)
     if issue:
       irc.privmsg(channel,issue)
